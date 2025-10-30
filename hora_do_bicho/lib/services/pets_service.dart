d47195c38e5c7dd8e9de8c460c899fced0dee2f4
Lisa <heloisamedeiros7@hotmail.com>
2025-10-30 19:21:12 -0300

    so um trem

--- hora_do_bicho/lib/services/pets_service.dart
+++ hora_do_bicho/lib/services/pets_service.dart
@@ -6,7 +6,7 @@ class PetsService {
   final String baseUrl = 'http://localhost:8080/pets';
 
   Future<List<Pet>> listarPets(int userId) async {
-    final response = await http.get(Uri.parse('$baseUrl/pets?userId=$userId'));
+    final response = await http.get(Uri.parse('$baseUrl/$userId'));
 
     if (response.statusCode == 200) {
       final List<dynamic> listaJson = jsonDecode(response.body);
